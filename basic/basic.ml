63bf90f9fe2058c50c13fb7a7971217db2235cc1
Nicholas Zuber <zuber.nicholas@gmail.com>
2016-12-24 12:13:33 -0500

    dedup

--- basic/basic.ml
+++ basic/basic.ml
@@ -29,7 +29,7 @@ let () = List.iter ~f:(printf "%d, ") lst
 let () = printf "\n"
 let () = printf "int:\t%d\n" (list_to_int lst)
 
-(* *)
+(* val avg_num : int list -> float = <fun> *)
 let avg_num lst = 
   let sum = List.fold lst 
   ~init:0
@@ -39,4 +39,46 @@ let avg_num lst =
 ;;
 
 let () = printf "sum:\t%f\n" (avg_num [1;2;5;3;6;2;8;3;4;2;1])
+
+(* val exists_in_list : 'a list -> bool *)
+let rec exists_in_list e l = 
+  match l with
+  | [] -> false
+  | hd :: tl -> if hd = e then true else (exists_in_list e tl)
+;;
+
+let () = printf "eil:\t%B\n" (exists_in_list 3 [1;2;3;4;5])
+let () = printf "eil:\t%B\n" (exists_in_list 6 [1;2;3;4;5])
+
+let dedup lst =
+  List.fold lst
+  ~init:[]
+  ~f:(fun acc x -> if (exists_in_list x acc) then acc else (x :: acc))
+;;
+
+let () = printf "dedup:\t"
+let () = List.iter ~f:(printf "%d, ") (dedup [1;2;3;4;5;5;6;1;2])
+let () = printf "\n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
