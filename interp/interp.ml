b95d241a4063a21c0e6493e3fc8a39f9ba7952c8
Nicholas Zuber <zuber.nicholas@gmail.com>
2016-12-27 16:04:45 -0500

    added cfwae definition

--- interp/interp.ml
+++ interp/interp.ml
@@ -1,3 +1,16 @@
 
-(* probably need to define some types like for Binding and Env etc *)
+(* probably need to define some types like for Binding and Env etc
+
+ CFWAE = number
+       |   (+ CFWAE CFWAE)
+       |   (- CFWAE CFWAE)
+       |   (* CFWAE CFWAE)
+       |   (/ CFWAE CFWAE)
+       |   id
+       |   (if0 CFWAE CFWAE CFWAE)
+       |   (with ([id CFWAE] ...) CFWAE)
+       |   (fun (id ...) CFWAE)
+       |   (CFWAE CFWAE ...)
+
+*)
 
