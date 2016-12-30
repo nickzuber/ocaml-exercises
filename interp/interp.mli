a283aa9c7bc9500fa47a854c1cc89295dd289b25
Nicholas Zuber <zuber.nicholas@gmail.com>
2016-12-30 16:47:22 -0500

    better interface

--- interp/interp.mli
+++ interp/interp.mli
@@ -1,30 +1,41 @@
 
 (* Functions, with, and arithmetic expressions *)
-type cfwae = 
+type sexp = 
   | Num of int
   | Binop
   | With
   | Id of string
-  | Fun
-  | App
+  | Fun of string list * sexp
+  | App of sexp * sexp list
 
-(* Interpreted value for a cfwae *)
-type cfwae_value =
+(*  *)
+type closure = {
+  params: string list; (* the list of identifiers *)
+  body: sexp;          (* body expression *)
+  env: env;            (* function's scope *)
+}
+
+(* Interpreted value for a sexp *)
+type sexp_value =
   | NumV of int
-  | ClosureV
+  | ClosureV of closure
 
 (* Binding from symbol to its value *)
 type binding = {
   name: string;
-  value: cfwae;
+  value: sexp;
 }
 
 (* Empty environment *)
-type mt
+type mt_env = Empty
 
 (* Environment scoping *)
-type env = {
+type an_env = {
   name: string;
-  value: cfwae_value;
+  value: sexp_value;
   next: env;
 }
+
+type env = 
+  | MtEnv of mt_env
+  | AnEnv of an_env
