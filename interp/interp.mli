9a4e8fc7321ce9a5f168bb4f7ce7df4a84a20d12
Nicholas Zuber <zuber.nicholas@gmail.com>
2016-12-30 13:19:25 -0500

    fixed formatting nit

--- interp/interp.mli
+++ interp/interp.mli
@@ -1,30 +1,30 @@
 
 (* Functions, with, and arithmetic expressions *)
 type cfwae = 
-  | Num
+  | Num of int
   | Binop
   | With
-  | Id
+  | Id of string
   | Fun
   | App
 
 (* Interpreted value for a cfwae *)
 type cfwae_value =
-  | NumV
+  | NumV of int
   | ClosureV
 
 (* Binding from symbol to its value *)
-type binding = 
-  { name  : string;
-    value : cfwae;
-  }
+type binding = {
+  name: string;
+  value: cfwae;
+}
 
 (* Empty environment *)
 type mt
 
 (* Environment scoping *)
-type env = 
-  { name  : string;
-    value : cfwae_value;
-    next  : env;
-  }
+type env = {
+  name: string;
+  value: cfwae_value;
+  next: env;
+}
