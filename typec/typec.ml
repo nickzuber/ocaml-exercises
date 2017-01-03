db21a8160a825f729f9ebc37bab1586d4a9ff8b7
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-01-03 16:28:30 -0500

    optimized type matching and added Bif

--- typec/typec.ml
+++ typec/typec.ml
@@ -42,19 +42,13 @@ let sampleAST =
           (Num 2))))
 
 let simpleAST = 
-  Binop (Plus, (Num 1), (Bool true))
+  Bif ((Binop (Plus, (Num 1), (Num 2))), (Bool false), (Bool true))
 
 let rec resolve id env = 
   match env with
   | [] -> Null
   | bind :: rest when bind.name = id -> bind.value
   | bind :: rest -> resolve id rest
-
-(* Checks if a given type is a T_num *)
-let isNum t = 
-  match t with
-  | T_num -> true
-  | _ -> false
 
 let typeOf expr = 
   let rec getType e env = 
@@ -66,12 +60,20 @@ let typeOf expr =
     | Binop (ops, lhs, rhs) -> 
         let lhs' = getType lhs env in
         let rhs' = getType rhs env in
-        if isNum lhs' then
-          if isNum rhs' then
+        if lhs' = T_num then
+          if rhs' = T_num then
             T_num
           else T_error "Binop right hand side was not a number."
         else T_error "Binop left hand side was not a number."
-    | Bif (cond, th, el) -> T_error "Not implemented"
+    | Bif (cond, th, el) -> 
+        let cond' = getType cond env in
+        let th' = getType th env in
+        let el' = getType el env in
+        if cond' = T_bool then
+          if th' = el' then
+            th' (* Since both th' and el' are equal, it doesn't matter which one we return here*)
+          else T_error "Bif branches were of different types."
+        else T_error "Bif condition was not a boolean."
     | With (id, value, body) -> T_error "Not implemented"
     | App (body, args) -> T_error "Not implemented"
     | Fun (param, body) -> T_error "Not implemented"
