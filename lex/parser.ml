9ac6e9d6f21a8f03b9f6ad12b1b402e6731dd63a
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-01-29 23:22:01 -0500

    more lexing progress, trying to plan

--- lex/parser.ml
+++ lex/parser.ml
@@ -1,10 +1,23 @@
 
+module Token = Lexer.Token
+
+(* GENERAL NOTES ABOUT STRUCTURE:
+ * 
+ * The Lexer itself defines an environment
+ *  - Maybe here is where the lexer position and process is tracked and handled?
+ *  - I've noticed this env is being passed around after each token iteration,
+ *    this must be the source of truth for lexing?
+ *
+ * 
+ *)
+
 let _ =
   try
+    let open Printf in
     let lexbuf = Lexing.from_channel stdin in
     let result = Lexer.token lexbuf in
-    let open Printf in
-    printf "%s\n" (Lexer.Token.token_to_string result)
+    printf "%s\n" (Token.token_to_string result);
+    printf "lexbuf: %d\n" (Lexing.lexeme_start lexbuf)
 (* 
     List.iter (fun tok -> printf "%s\n" (Lexer.Token.token_to_string tok)) result 
 *)
