5f120f565b3fe507304b04382c163ac9dd035aff
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-02-01 23:39:34 -0500

    reading files

--- lex/parser.ml
+++ lex/parser.ml
@@ -7,16 +7,17 @@ module Token = Lexer.Token
  *  - Maybe here is where the lexer position and process is tracked and handled?
  *  - I've noticed this env is being passed around after each token iteration,
  *    this must be the source of truth for lexing?
- *
  *)
+
+let filename = Sys.argv.(1)
 
 let _ =
   try
-    let open Printf in
-    let lexbuf = Lexing.from_channel stdin in
-    let result = Lexer.token lexbuf in
-    printf "%s\n" (Token.token_to_string result);
-    printf "lexbuf: %d\n" (Lexing.lexeme_start lexbuf)
+    let input = open_in filename in
+    let filebuf = Lexing.from_input input in
+    let result = Lexer.token filebuf in
+    Printf.printf "%s\n" (Token.token_to_string result);
+    Printf.printf "lexbuf: %d\n" (Lexing.lexeme_start lexbuf)
   with Lexer.Eof ->
     exit 0
 
