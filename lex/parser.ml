e1bed67db91a6df908ccd3e6ac22bdcc5e0f37c0
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-02-09 01:31:19 -0500

    dont need dis no mo

--- lex/parser.ml
+++ lex/parser.ml
@@ -2,14 +2,6 @@
 module Token = Lexer.Token
 open Batteries
 
-(* GENERAL NOTES ABOUT STRUCTURE:
- * 
- * The Lexer itself defines an environment
- *  - Maybe here is where the lexer position and process is tracked and handled?
- *  - I've noticed this env is being passed around after each token iteration,
- *    this must be the source of truth for lexing?
- *)
-
 let filename = Sys.argv.(1)
 
 let env = []
