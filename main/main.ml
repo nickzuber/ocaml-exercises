985445eb1a0287fc60532a4b6cabd020c6ac0ddf
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-10-04 19:45:47 -0400

    Fail at trying to separate recursive modules

--- main/main.ml
+++ main/main.ml
@@ -1,9 +1,29 @@
 
-module rec TestMethod : sig
-  type t = A of int
-  [@@deriving show]
-end = TestMethod
+module B_DEFINITION = struct
+  let bar x = x
+    |> A.foo
+    |> string_of_int
+  let baz x = x + 10
+end
 
-let test = TestMethod.A 123
+module A_DEFINITION = struct
+  let foo x = 
+    let n = x + 1
+    in B.baz n
+end
 
-let _ = Format.printf "%a@." TestMethod.pp test
+
+(* --- *)
+
+module rec A : sig
+  val foo : int -> int
+end = A_DEFINITION
+
+and B : sig
+  val bar : int -> string
+  val baz : int -> int
+end = B_DEFINITION
+
+let test = B.bar 1
+
+let _ = Printf.printf "result: %s\n" test 
